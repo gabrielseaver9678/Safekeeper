b9b0799885e4350337d02ec287dc9ccbef3ac810
Gabriel Seaver <gabrielseaver9678@gmail.com>
2021-06-12 19:11:14 -0400

    layouts fixed

--- safekeeper/gui/layout/MatchedPairsLayout.java
+++ safekeeper/gui/layout/MatchedPairsLayout.java
@@ -1,40 +1,57 @@
+
+// MatchedPairsLayout.java, Gabriel Seaver, 2021
+
 package safekeeper.gui.layout;
 
 import java.awt.Component;
 import java.awt.Container;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.swing.GroupLayout;
 
 public class MatchedPairsLayout extends GroupLayout {
-	private final List<Component> leftComponents = new ArrayList<>(), rightComponents = new ArrayList<>();
 	
-	public MatchedPairsLayout(Container paramContainer) {
-		super(paramContainer);
+	private final List<Component>
+		leftComponents = new ArrayList<Component>(),
+		rightComponents = new ArrayList<Component>();
+	
+	public MatchedPairsLayout (Container host) {
+		super(host);
 	}
 	
-	public void addMatch(Container paramContainer1, Container paramContainer2) {
-		this.leftComponents.add(paramContainer1);
-		this.rightComponents.add(paramContainer2);
+	public void addMatch (Component left, Component right) {
+		leftComponents.add(left);
+		rightComponents.add(right);
 	}
 	
-	public void finalizeLayout() {
-		int i = this.leftComponents.size();
-		GroupLayout.SequentialGroup sequentialGroup1 = createSequentialGroup();
-		GroupLayout.ParallelGroup parallelGroup1 = createParallelGroup(GroupLayout.Alignment.TRAILING);
-		for (Component component : this.leftComponents)
-			parallelGroup1.addComponent(component);
-		sequentialGroup1.addGroup(parallelGroup1);
-		GroupLayout.ParallelGroup parallelGroup2 = createParallelGroup(GroupLayout.Alignment.LEADING);
-		for (Component component : this.rightComponents)
-			parallelGroup2.addComponent(component);
-		sequentialGroup1.addGroup(parallelGroup2);
-		setHorizontalGroup(sequentialGroup1);
-		GroupLayout.SequentialGroup sequentialGroup2 = createSequentialGroup();
-		for (byte b = 0; b < i; b++)
-			sequentialGroup2.addGroup(createParallelGroup(GroupLayout.Alignment.CENTER)
-					.addComponent(this.leftComponents.get(b))
-					.addComponent(this.rightComponents.get(b)));
-		setVerticalGroup(sequentialGroup2);
+	public void finalizeLayout () {
+		final int rows = leftComponents.size();
+		
+		// Make the horizontal group
+		SequentialGroup horizontalGroup = createSequentialGroup();
+		
+		// Left vertical group
+		ParallelGroup leftGroup = createParallelGroup(Alignment.TRAILING);
+		for (Component component : leftComponents)
+			leftGroup.addComponent(component);
+		horizontalGroup.addGroup(leftGroup);
+		
+		// Right vertical group
+		ParallelGroup rightGroup = createParallelGroup(Alignment.LEADING);
+		for (Component component : rightComponents)
+			rightGroup.addComponent(component);
+		horizontalGroup.addGroup(rightGroup);
+		
+		setHorizontalGroup(horizontalGroup);
+		
+		// Make the vertical group
+		SequentialGroup verticalGroup = createSequentialGroup();
+		for (int row = 0; row < rows; row ++)
+			verticalGroup.addGroup(createParallelGroup(Alignment.CENTER)
+				.addComponent(leftComponents.get(row))
+				.addComponent(rightComponents.get(row)));
+		setVerticalGroup(verticalGroup);
 	}
+	
 }
--- safekeeper/gui/layout/RowsLayout.java
+++ safekeeper/gui/layout/RowsLayout.java
@@ -1,40 +1,48 @@
+
+// RowsLayout.java, Gabriel Seaver, 2021
+
 package safekeeper.gui.layout;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
+
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+
 import safekeeper.gui.util.GUIUtils;
 
 public class RowsLayout extends BoxLayout {
+	
 	private final Container host;
+	private final int hgap, vgap;
 	
-	private final int hgap;
-	
-	private final int vgap;
-	
-	public RowsLayout(Container paramContainer) {
-		this(paramContainer, 0, 0);
+	public RowsLayout (Container host) {
+		this(host, 0, 0);
 	}
 	
-	public RowsLayout(Container paramContainer, int paramInt1, int paramInt2) {
-		super(paramContainer, 1);
-		this.host = paramContainer;
-		this.hgap = paramInt1;
-		this.vgap = paramInt2;
+	public RowsLayout (Container host, int hgap, int vgap) {
+		super(host, BoxLayout.Y_AXIS);
+		this.host = host;
+		this.hgap = hgap;
+		this.vgap = vgap;
 		addVerticalSpacer();
 	}
 	
-	private void addVerticalSpacer() {
-		this.host.add(GUIUtils.makeVerticalStrut(this.vgap));
+	private void addVerticalSpacer () {
+		host.add(GUIUtils.makeVerticalStrut(vgap));
 	}
 	
-	public void addRow(JComponent... paramVarArgs) {
-		JPanel jPanel = new JPanel(new FlowLayout(1, this.hgap, 0));
-		for (JComponent jComponent : paramVarArgs)
-			jPanel.add(jComponent);
-		this.host.add(jPanel);
+	public void addRow (JComponent... components) {
+		// Create a new row panel
+		JPanel rowPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, hgap, 0));
+		
+		// Add all row components to the row panel
+		for (JComponent component : components)
+			rowPanel.add(component);
+		
+		// Add row panel and next vertical spacer
+		host.add(rowPanel);
 		addVerticalSpacer();
 	}
 }
