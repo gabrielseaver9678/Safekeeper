8755d797f0f96905992f2812493045943e47812b
Gabriel Seaver <gabrielseaver9678@gmail.com>
2021-06-12 21:57:25 -0400

    Compiled in version 11.0.11

--- compile.bat
+++ compile.bat
@@ -17,7 +17,7 @@ mkdir Safekeeper\
 rem Java compilation
 echo Java compilation
 cd ..\src\
-javac -d ..\compiled\classes safekeeper\Entry.java
+"C:\Program Files\Java\jdk-11.0.11\bin\javac.exe" -d ..\compiled\classes safekeeper\Entry.java
 cd ..\compiled\
 
 
@@ -29,7 +29,7 @@ xcopy /E ..\src\resources\ Safekeeper\resources\
 rem Creating jar
 echo Creating jar
 cd classes\
-jar cfe Safekeeper.jar safekeeper.Entry safekeeper\
+"C:\Program Files\Java\jdk-11.0.11\bin\jar.exe" cfe Safekeeper.jar safekeeper.Entry safekeeper\
 move Safekeeper.jar ..\Safekeeper\Safekeeper.jar
 cd ..\
 
