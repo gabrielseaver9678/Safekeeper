ca8872bb9274ccdcac472d436711e9937488ad6b
Gabriel Seaver <gabrielseaver9678@gmail.com>
2021-06-12 23:45:20 -0400

    Removed unusable executables

--- compile.bat
+++ compile.bat
@@ -10,36 +10,27 @@ echo Recreating compiled directory
 mkdir compiled\
 cd compiled\
 mkdir classes\
-mkdir Safekeeper_Java\
-mkdir Safekeeper_Windows\
+mkdir Safekeeper\
 
 
 rem Java compilation
 echo Java compilation
 cd ..\src\
-"C:\Program Files\Java\jdk-11.0.11\bin\javac.exe" -d ..\compiled\classes safekeeper\Entry.java
+javac -d ..\compiled\classes safekeeper\Entry.java
 cd ..\compiled\
 
 
 rem Creating jar
 echo Creating jar
 cd classes\
-"C:\Program Files\Java\jdk-11.0.11\bin\jar.exe" cfe Safekeeper.jar safekeeper.Entry safekeeper\
-move Safekeeper.jar ..\Safekeeper_Java\Safekeeper.jar
+jar cfe Safekeeper.jar safekeeper.Entry safekeeper\
+move Safekeeper.jar ..\Safekeeper\Safekeeper.jar
 cd ..\
 
 
 rem Copying resources
 echo Copying resources
-xcopy /E ..\src\resources\ Safekeeper_Java\resources\
-xcopy /E ..\src\resources\ Safekeeper_Windows\resources\
-
-
-rem Creating windows executable
-echo Creating windows executable
-cd ..\
-launch4jc launch4j_config.xml
-cd compiled\
+xcopy /E ..\src\resources\ Safekeeper\resources\
 
 
 pause
